feaa0311fbacabddea83ded94a6eca52acd8482b
Hayden <44925247+Koalacards@users.noreply.github.com>
2021-08-17 22:57:20 -0400

    Adding images on the fly

--- bot.py
+++ bot.py
@@ -8,13 +8,30 @@ from discord_slash_components import DiscordComponents
 from confidential import RUN_ID, SUGGESTION_CHANNEL
 import dbfunc
 import json
+import random
 from timeit import default_timer as timer
 
 client = commands.Bot(".")
 slash= SlashCommand(client, sync_commands=True, override_type=True)
 
 invite_button = Button(label="Invite", style=ButtonStyle.URL, url="https://discord.com/oauth2/authorize?client_id=876097748255014932&permissions=2147567616&scope=bot%20applications.commands")
-support_button = Button(label="Support", style=ButtonStyle.URL, url="https://discord.gg/5Jn32Upk4M")
+support_button = Button(label="Support", style=ButtonStyle.URL, url="https://discord.gg/5Jn32Upk4M")\
+
+kinda_sus_pictures=["https://i.etsystatic.com/26195327/r/il/b9103b/2797002083/il_fullxfull.2797002083_t400.jpg",
+ "https://imgix.bustle.com/uploads/image/2020/10/31/31aa14f0-bc99-4e6b-b785-26ae420971dd-screen-shot-2020-10-31-at-52151-pm.png?w=1200&h=630&fit=crop&crop=faces&fm=jpg",
+ "https://image-cdn.neatoshop.com/styleimg/113537/none/black/default/477968-20;1605375643x.jpg",
+ "https://ih1.redbubble.net/image.1042054698.6192/st,small,507x507-pad,600x600,f8f8f8.jpg",
+ "https://cdn.shopify.com/s/files/1/0324/7941/2283/products/LookinKindaSus1_740x.jpg?v=1607627480",
+ "http://www.rogueduck.art/uploads/1/2/4/7/124740057/s829560345432686266_p124_i1_w450.png",
+ "https://imageproxy.ifunny.co/crop:x-20,resize:640x,quality:90x75/images/109afed84e8549231a40801bc7071ba2e2eaffb52863c22cafd6d5c631839d10_1.jpg",
+ "https://i1.sndcdn.com/artworks-BZOdK0DGnlFHa23y-jG4Vcw-t500x500.jpg",
+ "https://i.etsystatic.com/25325402/r/il/39e5de/2600613663/il_570xN.2600613663_mmum.jpg",
+ "https://res.cloudinary.com/teepublic/image/private/s--g97Q4qXJ--/t_Resized%20Artwork/c_fit,g_north_west,h_954,w_954/co_000000,e_outline:35/co_000000,e_outline:inner_fill:35/co_ffffff,e_outline:35/co_ffffff,e_outline:inner_fill:35/co_bbbbbb,e_outline:3:1000/c_mpad,g_center,h_1260,w_1260/b_rgb:eeeeee/c_limit,f_auto,h_630,q_90,w_630/v1601510568/production/designs/14563420_0.jpg"
+ "https://i.imgflip.com/4kg413.jpg",
+ "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRsKHUFDoYcyODaSg9NOS_gbHrUvx428plJ0w&usqp=CAU",
+ "https://res.cloudinary.com/teepublic/image/private/s--WaNQLAIU--/t_Resized%20Artwork/c_fit,g_north_west,h_1054,w_1054/co_ffffff,e_outline:53/co_ffffff,e_outline:inner_fill:53/co_bbbbbb,e_outline:3:1000/c_mpad,g_center,h_1260,w_1260/b_rgb:eeeeee/c_limit,f_auto,h_630,q_90,w_630/v1606090299/production/designs/9596239_1.jpg",
+ "https://images.fineartamerica.com/images/artworkimages/mediumlarge/3/among-us-being-very-sus-trung-dinh-art.jpg",
+ "https://pbs.twimg.com/media/E2IGViFXMAAyldO.png"]
 
 buttons = [invite_button, support_button]
 
@@ -67,7 +84,12 @@ async def sus_o_meter(ctx):
         
         count+=1 
 
-    await ctx.send(embed=_create_embed(title, description, colour), components=buttons)
+    embed = _create_embed(title, description, colour)
+
+    if len(sus_dict.values()) == 0:
+        embed.set_image(url=kinda_sus_pictures[random.randint(0, len(kinda_sus_pictures - 1))])
+
+    await ctx.send(embed=embed, components=buttons)
 
 @slash.slash(name='sus-words', description="Find out what words make people sus!")
 async def sus_words(ctx):
