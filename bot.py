36f10a1f72fb13948b003b8478030c7fbee17fbd
Hayden <44925247+Koalacards@users.noreply.github.com>
2022-01-08 12:01:08 -0500

    Update server count automatically
    Runs a task that updates the server count every 30 minutes automatically in the title and on top.gg

--- bot.py
+++ bot.py
@@ -1,10 +1,13 @@
 import discord
 from discord.ext import commands
+from discord.ext import tasks
 from discord_slash import SlashCommand
-from confidential import RUN_ID
+from confidential import RUN_ID, DBL_TOKEN
+import topgg
 
 client = commands.Bot(".")
 slash= SlashCommand(client, sync_commands=True, override_type=True)
+client.topggpy = topgg.DBLClient(client, DBL_TOKEN)
 
 client.load_extension('cogs.admincommands')
 client.load_extension('cogs.meter')
@@ -12,7 +15,8 @@ client.load_extension('cogs.customlists')
 
 @client.event
 async def on_ready():
-    await client.change_presence(activity=discord.Game(name="/help | Now with custom lists!"))
+    guild_count = str(len(client.guilds))
+    await client.change_presence(activity=discord.Game(name=f"/help in {guild_count} servers | Now with custom lists!"))
     print("ready")
 
 @client.command()
@@ -22,5 +26,16 @@ async def reloadCog(ctx, cog):
         await ctx.send("Cog has been reloaded")
     else:
         await ctx.send("You are not my creator")
+
+@tasks.loop(minutes=30)
+async def update_stats():
+    guild_count = str(len(client.guilds))
+    await client.change_presence(activity=discord.Game(name=f"/help in {guild_count} servers | Now with custom lists!"))
+    try:
+        await client.topggpy.post_guild_count()
+        print(f"Posted server count ({client.topggpy.guild_count})")
+    except Exception as e:
+        print(f"Failed to post server count\n{e.__class__.__name__}: {e}")
+
 
 client.run(RUN_ID)
